22da6c382b0f335d8b8a27fbd60d8720a82caf7b
romajc84 <joeyroman84@gmail.com>
2020-07-11 15:08:44 -0400

    Phrase-O-Matic

--- JavaTest.java
+++ JavaTest.java
@@ -1,29 +1,24 @@
 public class JavaTest {
 
     public static void main(String[] args) {
-        int beerNum = 99;
-        String word = "bottles";
+        
+        String[] wordListOne = { "random-words", "another-word", "yet-another" };
 
-        while (beerNum > 0) {
+        String[] wordListTwo = { "still", "kelsey", "Lucas" };
 
-            if (beerNum == 1) {
-                word = "bottle";
-            }
+        String[] wordListThree = { "this", "book", "is", "good" };
 
-            System.out.println(beerNum + " " + word + " of beer on the wall.");
-            System.out.println(beerNum + " " + word + " of beer.");
-            System.out.println("Take one down.");
-            System.out.println("Pass it around.");
-            beerNum = beerNum - 1;
+        int oneLength = wordListOne.length;
+        int twoLength = wordListTwo.length;
+        int threeLength = wordListThree.length;
 
-            if (beerNum > 1) {
-                System.out.println(beerNum + " " + word + " of beer on the wall.");
-            }
-            if (beerNum == 1) {
-                System.out.println(beerNum + " bottle of beer on the wall.");
-            }
-        }
-        System.out.println("No more bottles of beer on the wall.");
+        int rand1 = (int) (Math.random() * oneLength);
+        int rand2 = (int) (Math.random() * twoLength);
+        int rand3 = (int) (Math.random() * threeLength);
+
+        String phrase = wordListOne[rand1] + " " + wordListTwo[rand2] + " " + wordListThree[rand3];
+
+        System.out.println("What we need is a " + phrase);
 
     }
 }
