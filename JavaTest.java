3b74460b8b0d5c10624427bae78e49cbf959b888
romajc84 <joeyroman84@gmail.com>
2020-07-11 14:58:02 -0400

    99 Bottles

---
+++ JavaTest.java
@@ -0,0 +1,29 @@
+public class JavaTest {
+
+    public static void main(String[] args) {
+        int beerNum = 99;
+        String word = "bottles";
+
+        while (beerNum > 0) {
+
+            if (beerNum == 1) {
+                word = "bottle";
+            }
+
+            System.out.println(beerNum + " " + word + " of beer on the wall.");
+            System.out.println(beerNum + " " + word + " of beer.");
+            System.out.println("Take one down.");
+            System.out.println("Pass it around.");
+            beerNum = beerNum - 1;
+
+            if (beerNum > 1) {
+                System.out.println(beerNum + " " + word + " of beer on the wall.");
+            }
+            if (beerNum == 1) {
+                System.out.println(beerNum + " bottle of beer on the wall.");
+            }
+        }
+        System.out.println("No more bottles of beer on the wall.");
+
+    }
+}
