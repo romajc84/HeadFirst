9844ab9fdb919b74b72ab97876e03fc5a3bfbf35
romajc84 <joeyroman84@gmail.com>
2020-07-12 10:17:29 -0400

    Code Magnets

--- JavaTest.java
+++ JavaTest.java
@@ -2,23 +2,24 @@ public class JavaTest {
 
     public static void main(String[] args) {
         
-        String[] wordListOne = { "random-words", "another-word", "yet-another" };
+        int x = 3;
 
-        String[] wordListTwo = { "still", "kelsey", "Lucas" };
-
-        String[] wordListThree = { "this", "book", "is", "good" };
-
-        int oneLength = wordListOne.length;
-        int twoLength = wordListTwo.length;
-        int threeLength = wordListThree.length;
-
-        int rand1 = (int) (Math.random() * oneLength);
-        int rand2 = (int) (Math.random() * twoLength);
-        int rand3 = (int) (Math.random() * threeLength);
-
-        String phrase = wordListOne[rand1] + " " + wordListTwo[rand2] + " " + wordListThree[rand3];
-
-        System.out.println("What we need is a " + phrase);
+        while (x > 0) {
+            if (x > 2) {
+                System.out.print("a");
+                x = x - 1;
+                System.out.print("-");
+            }
+            if (x == 2) {
+                System.out.print("b c");
+                x = x - 1;
+                System.out.print("-");
+            }
+            if (x == 1) {
+                System.out.print("d");
+                x = x - 1;
+            }
+        }
 
     }
 }
