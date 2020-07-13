90a91f4fbd91ba968dc0ebdb1f43cdacd971627d
romajc84 <joeyroman84@gmail.com>
2020-07-13 15:41:47 -0400

    Code Magnets Islands (p.64)

--- JavaTest.java
+++ JavaTest.java
@@ -1,31 +1,31 @@
-
-
 class JavaTest {
 
     public static void main(String[] args) {
 
-        Echo e1 = new Echo();
-        Echo e2 = new Echo();
-        int x = 0;
-        while(x < 4) {
-            e1.hello();
-            e1.count = e1.count + 1;
-            if(x == 3) {
-                e2.count = e2.count + 1;
-            }
-            if(x > 0) {
-                e2.count = e2.count + e1.count;
-            }
-            x = x + 1;
+        String[] islands = new String[4];
+
+        islands[0] = "Bermuda";
+        islands[1] = "Fiji";
+        islands[2] = "Azores";
+        islands[3] = "Cozmule";
+
+        int[] index = new int[4];
+
+        index[0] = 1;
+        index[1] = 3;
+        index[2] = 0;
+        index[3] = 2;
+
+        int y = 0;
+
+        int ref;
+        while (y < 4) {
+            ref = index[y];
+
+            System.out.print("island = ");
+            System.out.println(islands[ref]);
+
+            y = y + 1;
         }
-        System.out.println(e2.count);
-    }
-}
-
-class Echo {
-    int count = 0;
-
-    void hello() {
-        System.out.println("helloooo...");
     }
 }
